7d49ff3195621001edbf02309b442ad6638947e9
Vlatko Kosturjak <kost@linux.hr>
2015-11-13 17:05:58 +0100

    Better error handling for modules

--- scan/manage-scan.rb
+++ scan/manage-scan.rb
@@ -29,6 +29,8 @@ def check_connection
 end
 
 def scan (url, opts, logger)
+	failed=Array.new
+
 	if url != nil then
 		Docker.url = url
 	end
@@ -42,40 +44,71 @@ def scan (url, opts, logger)
 	@log.info("Loading discovery modules...")
 
 	Dir["./modules/discover/*.rb"].each do |f| 
-		@log.info("Loading #{f}")
-		require f 
+		@log.info("Loading discovery module: #{f}")
+		begin
+			require f 
+		rescue SyntaxError => se
+			@log.info("Error loading audit module: #{f}")
+			@log.debug("Error executing audit module: #{se.backtrace}")
+			failed << f
+		end
 	end
 
 	@log.info("Running discovery modules...")
 	DiscoverModule.modules.each do |modclass|
 		@log.info("Running discovery module: #{modclass.name}")
-		mod=modclass.new
-		mod.scandata=@auditoutput
-		@auditoutput[mod.class.name]=mod.run
+		begin
+			mod=modclass.new
+			mod.scandata=@auditoutput
+			@auditoutput[mod.class.name]=mod.run
+		rescue Exception => e
+			@log.info("Error executing audit module: #{modclass.name}")
+			@log.debug("Error executing audit module: #{e.backtrace}")
+			failed << modclass.name
+		end
 		# pp @auditoutput[mod.class.name]
 	end
 
 
 	@log.info("Loading audit modules...")
 	Dir["./modules/audit/*.rb"].each do |f| 
-		@log.info("Loading #{f}")
-		require f 
+		@log.info("Loading audit module: #{f}")
+		begin
+			require f 
+		rescue SyntaxError => se
+			@log.info("Error loading audit module: #{f}")
+			@log.debug("Error executing audit module: #{se.backtrace}")
+			failed << f
+		end
 	end
 
 	@log.info("Running audit modules...")
 	AuditModule.modules.each do |modclass|
 		@log.info("Running audit module: #{modclass.name}")
-		mod=modclass.new
-		mod.scandata=@auditoutput
-		@auditoutput[mod.class.name]=mod.check('test')
+		begin
+			mod=modclass.new
+			mod.scandata=@auditoutput
+			@auditoutput[mod.class.name]=mod.check('test')
+		rescue Exception => e
+			@log.info("Error executing audit module: #{modclass.name}")
+			@log.debug("Error executing audit module: #{e.backtrace}")
+			failed << modclass.name
+		end
 		# pp @auditoutput[mod.class.name]
 	end
 
 	@log.info("Loading report modules...")
 	Dir["./modules/report/*.rb"].each do |f| 
 		@log.info("Loading report #{f}")
-		require f 
+		begin
+			require f 
+		rescue SyntaxError => se
+			@log.info("Error loading report module: #{f}")
+			@log.debug("Error executing report module: #{se.backtrace}")
+			failed << f
+		end
 	end
+
 
 	@log.info("Running report modules...")
 	ReportModule.modules.each do |modclass|
@@ -88,20 +121,34 @@ def scan (url, opts, logger)
 		end
 		formats.each do |fmt|
 			if fmt==mod.format then
-				mod.scandata=@auditoutput
-				output=mod.report(nil)
-				if opts.key?("output") then
-					reportfilename = opts["output"]
-					reportfilename << mod.file_extension
-					File.open(reportfilename, 'w') { |file| file.write(output) }
-				else
-					puts output	
+				begin
+					mod.scandata=@auditoutput
+					output=mod.report(nil)
+					if opts.key?("output") then
+						reportfilename = opts["output"]
+						reportfilename << mod.file_extension
+						File.open(reportfilename, 'w') { |file| file.write(output) }
+					else
+						puts output	
+					end
+				rescue Exception => e
+					@log.info("Error executing report module: #{modclass.name}")
+					@log.debug("Error executing report module: #{e.backtrace}")
+					failed << modclass.name
 				end
 				@log.debug(output)
 			else
 				@log.debug("Skipping report module: #{modclass.name}");
 			end
 		end
+	end
+
+	if failed.count > 0
+		failedstr=""
+		failed.each do |f|
+			failedstr = f + " "
+		end
+		@log.warn("Following modules failed: #{failedstr}") 
 	end
 end
 
